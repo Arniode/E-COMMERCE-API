1e60e4fac811cbf7b411abea36afb6110dc6141b
Arniode <toluwanimiadediran2@gmail.com>
2026-04-28 23:47:23 +0100

    Added Update and Delete logic to Controller

--- controllers/productController.js
+++ controllers/productController.js
@@ -19,3 +19,29 @@ exports.createProduct = async (req, res) => {
         res.status(400).json({ status: "error", message: "Check your data format" });
     }
 };
+
+
+// Function to UPDATE a product
+exports.updateProduct = async (req, res) => {
+    try {
+        const product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true
+        });
+        if (!product) return res.status(404).json({ message: "Product not found" });
+        res.status(200).json({ status: "Success", data: product });
+    } catch (err) {
+        res.status(400).json({ status: "Fail", message: err.message });
+    }
+};
+
+// Function to DELETE a product
+exports.deleteProduct = async (req, res) => {
+    try {
+        const product = await Product.findByIdAndDelete(req.params.id);
+        if (!product) return res.status(404).json({ message: "Product not found" });
+        res.status(200).json({ status: "Success", message: "Product deleted successfully" });
+    } catch (err) {
+        res.status(400).json({ status: "Fail", message: err.message });
+    }
+};
